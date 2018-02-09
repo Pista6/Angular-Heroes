79110cc9e6c2481417826bd580f0a6d081786af2
kalfas13 <stefandano6@gmail.com>
2018-02-09 19:07:06 +0100

    Heroes
    Thirth push. Adding List of heroes, rendering, icons and mouse actions (hero.component.css)

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import {Hero} from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Milan' },
+  { id: 12, name: 'Junak' },
+  { id: 13, name: 'Bambi' },
+  { id: 14, name: 'Celebrita' },
+  { id: 15, name: 'King' },
+  { id: 16, name: 'Man' },
+  { id: 17, name: 'Bad' },
+  { id: 18, name: 'lol' },
+  { id: 19, name: 'Nap' },
+  { id: 20, name: 'Bor' }
+];
+
